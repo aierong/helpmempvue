401fe6e602c1530eb39b1546f683de74fc985d20
aierong <aierong@126.com>
2019-04-22 15:32:21 +0800

    company

--- src/common/BmobApi/users.js
+++ src/common/BmobApi/users.js
@@ -1,9 +1,62 @@
 /*
-作者: chenghao
-功能: js脚本
-*/
+ 作者: chenghao
+ 功能: js脚本
+ */
 
+import { UserTable } from '@/common/TableName.js';
 
+//添加用户
+export function adduser ( registerUser ) {
 
+  return new Promise( ( resolve , reject ) => {
 
+    const query = Bmob.Query( UserTable );
+    //这里 设置  列的数据
 
+    query.set( "userid" , registerUser.userid )
+    query.set( "username" , registerUser.username )
+    query.set( "pwd" , registerUser.pwd )
+
+    query.save().then( res => {
+      //console.log( res )
+
+      resolve( res );
+      //返回创建时间和id
+      // {
+      //     createdAt: "YYYY-mm-dd HH:ii:ss",
+      //         objectId: "objectId"
+      // }
+    } ).catch( err => {
+      //console.log( err )
+
+      resolve( null );
+    } )
+  } );
+}
+
+//是存在用户id 存在手机号码
+export function isexistsuserid ( userid ) {
+  return new Promise( ( resolve , reject ) => {
+    const query = Bmob.Query( UserTable );
+    query.equalTo( "userid" , "==" , userid );
+
+    query.find().then( res => {
+      //返回的是数组,没有找到就是空数组
+      //console.log( res )
+
+      if ( res != null && res.length > 0 ) {
+        resolve( {
+          //是存在
+          isexists : true ,
+          data : res[ 0 ]
+        } );
+      }
+      else {
+        resolve( {
+          isexists : false ,
+          data : null
+        } );
+      }
+    } );
+  } );
+}
--- 项目资料/sql.sql
+++ 项目资料/sql.sql
@@ -1,4 +1,5 @@
 
+dl表结构:
 autokey  自动增长
 userid    录入人
 username  录入人姓名
@@ -31,105 +32,18 @@ addpmcreplydate     --pmc答复时间
     
 addpmcman         --pmc答复人
 addpmcmanname     --pmc答复人姓名
-	
     
 comment --备注 CS求助备注
- 
 
+overdate        --完成时间 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---新建一个表记录操作日志
-CREATE TABLE helpme_dllog
-(
-    autokey INT IDENTITY CONSTRAINT pk_helpme_dllog PRIMARY KEY ,
-    [CompanyId] VARCHAR(20) ,
-    -- add添加记录 update添加记录  reply答复  againhelp 再次求助
-    runtype NVARCHAR(16) ,
-
-    --状态
-    status VARCHAR(10) DEFAULT ( 'N' ) ,
-
-    --批核人
-    confirmid VARCHAR(20) ,
-
-    --批核日期
-    confirmdate DATETIME ,
-
-    --录入人
-    userid VARCHAR(20) ,
-
-    --录入日期
-    inputdate DATETIME ,
-
-    --修改人
-    modifyid VARCHAR(20) ,
-
-    --修改日期
-    modifydate DATETIME ,
-
-    -- 删除标志
-    delflag VARCHAR(20) ,
-
-    --工程单号
-    productno VARCHAR(88) ,
-    -- 客户编号
-    custno VARCHAR(88) ,
-    helppmcmans VARCHAR(1280) ,
-    helpnoworkmans VARCHAR(1280) ,
-
-    ----cs期望交期/或者pmc的复期
-    csexpectdate DATETIME NULL ,
-
-    --把当时的备注copy过来
-    [comment] [VARCHAR](720)
-)
-
-CREATE INDEX index_helpme_dllog
-    ON helpme_dllog ( CompanyId, productno, runtype, inputdate )
-
-
---当时的未完成工序代号,多个逗号分隔
-ALTER TABLE helpme_dllog
-ADD noworkprocessno VARCHAR(3600)
-
---当时的未完成工序名称,多个逗号分隔
-ALTER TABLE helpme_dllog
-ADD noworkprocessdesc VARCHAR(3600)
-
-
---程序编号  PC WX
-ALTER TABLE helpme_dllog 
-ADD ProgramNO     VARCHAR(20) DEFAULT('PC')
 
  
 
--- 备用1
-ALTER TABLE helpme_dllog 
-ADD Col1     VARCHAR(720) DEFAULT('')
-
--- 备用2
-ALTER TABLE helpme_dllog 
-ADD Col2     VARCHAR(720) DEFAULT('')
-
--- 备用3
-ALTER TABLE helpme_dllog 
-ADD Col3     VARCHAR(720) DEFAULT('')
+users表结构:
+autokey  自动增长
+userid   用户id (手机号)
+username 用户名 
+pwd      密码
 
 
