45276427e88ac7ce58869e1e32c5d910e97bdb30
aierong <aierong@126.com>
2019-04-24 17:43:18 +0800

    company

--- src/common/BmobApi/dllog.js
+++ src/common/BmobApi/dllog.js
@@ -3,7 +3,11 @@
  功能: js脚本
  */
 
+//  引入
+import Bmob from "hydrogen-js-sdk";
 
+//表名称
+import { DlLogTable } from '@/common/constant.js';
 
 
 
--- src/common/constant.js
+++ src/common/constant.js
@@ -8,9 +8,11 @@ export const UserTable = "users";
 
 //数据表
 export const DlTable = "dl";
+//日志表
+export const DlLogTable = "dllog";
 
 //本地存储的名称
 export const StorageName = "HelpMeStorage";
 
-//有效期
+//登录状态有效期
 export const validday = 7;
