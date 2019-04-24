a66d818201e791b4ed8b620c4442eab17f700374
aierong <aierong@126.com>
2019-04-24 17:53:05 +0800

    company

--- src/common/BmobApi/dllog.js
+++ src/common/BmobApi/dllog.js
@@ -9,5 +9,36 @@ import Bmob from "hydrogen-js-sdk";
 //表名称
 import { DlLogTable } from '@/common/constant.js';
 
+//添加dllog
+export function adddllog ( dllog ) {
+
+  return new Promise( ( resolve , reject ) => {
+
+    const query = Bmob.Query( DlLogTable );
+    //这里 设置  列的数据
+
+    query.set( "logruntype" , dllog.logruntype )
+    query.set( "userid" , dllog.userid )
+    query.set( "username" , dllog.username )
+    query.set( "productno" , dllog.productno )
+    query.set( "dates" , dllog.dates )
+    query.set( "comment" , dllog.comment )
+
+    query.save().then( res => {
+      //console.log( res )
+
+      resolve( res );
+      //返回创建时间和id
+      // {
+      //     createdAt: "YYYY-mm-dd HH:ii:ss",
+      //         objectId: "objectId"
+      // }
+    } ).catch( err => {
+      //console.log( err )
+
+      resolve( null );
+    } )
+  } );
+}
 
 
