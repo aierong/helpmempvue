4dce2f4058c802afb53feb505d6eccfe0c6b72cc
aierong <aierong@126.com>
2019-05-02 19:59:32 +0800

    company

---
+++ src/pages/againhelp/againhelp.vue
@@ -0,0 +1,51 @@
+<!--
+作者:chenghao
+Date: 2019/5/2
+Time: 19:44
+功能:
+-->
+
+<!-- html代码片段 -->
+<template>
+
+  <div>
+    againhelpagainhelpagainhelp
+  </div>
+
+</template>
+
+<!-- js脚本代码片段 -->
+<script>
+  export default {
+    name : "againhelp" ,
+    //数据模型
+    data () {
+      return {
+        msg : ''
+      }
+    } ,
+    //方法
+    methods : {
+      //methodsname() {
+      //代码搞这里
+      //},
+
+    } ,
+    //计算属性
+    computed : {
+      //name() {
+      //代码搞这里
+      //return this.data;
+      //}
+    } ,
+    //生命周期(mounted)
+    mounted () {
+
+    } ,
+  }
+</script>
+
+<!-- 样式代码片段  scoped -->
+<style src="./againhelp.css"
+       scoped>
+</style>
