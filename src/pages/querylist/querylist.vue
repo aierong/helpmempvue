444d2ffdc6c210a4c5f6625978a9628c29ba0879
aierong <aierong@126.com>
2019-05-10 18:03:48 +0800

    company

--- src/pages/querylist/querylist.vue
+++ src/pages/querylist/querylist.vue
@@ -22,6 +22,40 @@
                 show-action
                 @search="onSearch"
                 @cancel="onSearchCancel"/>
+
+
+    <!--
+    use-slot	是否使用自定义内容的插槽
+    close 关闭弹窗
+    -->
+    <van-dialog use-slot
+                :show="showdialog"
+                show-cancel-button
+                @close="onCloseDialog">
+      <!--      <view>dd</view> @change="onChange2"-->
+      <van-radio-group :value="radioval1"
+      >
+        <van-cell-group>
+          <van-cell title="我求助的单据"
+                    clickable
+                    data-name="myhelp"
+                    @click="onClickCell">
+            <van-radio name="myhelp"/>
+          </van-cell>
+          <van-cell title="求助我的单据"
+                    clickable
+                    data-name="helpmy"
+                    @click="onClickCell">
+            <van-radio name="helpmy"/>
+          </van-cell>
+        </van-cell-group>
+      </van-radio-group>
+    </van-dialog>
+    <!--
+注意要配一个van-dialog,才会显示提示 ,默认id van-dialog
+-->
+    <van-dialog id="van-dialog"/>
+
   </div>
 
 </template>
@@ -36,21 +70,47 @@
     //数据模型
     data () {
       return {
+        showdialog : false ,
+
         SearchVal : '' ,
         productlist : [] ,
       }
     } ,
     //方法
     methods : {
-      onselecttype () {
-        //下面这个方法到时要重写
-        let _data = 'myhelp';
+      onClickCell ( ee ) {
+        // ee.mp.currentTarget.dataset.name 可以取到  van-cell 中设置的 data-name="v2"
+        let val = ee.mp.currentTarget.dataset.name;
 
-        if ( this.userselectquerytype == 'myhelp' ) {
-          _data = 'helpme'
+        console.log( val )
+
+        let obj = this.$store.state.userselectquery;
+        obj.querytype = val;
+
+        this.$store.dispatch( 'UpdateUserSelectQuery' , obj );
+
+      } ,
+      onCloseDialog ( event ) {
+
+        if ( event.mp.detail === 'confirm' ) {
+          //点击了 确定按钮
+          // console.log( '点击确定' )
+
+          //把选择的状态写入vuex
+
+          //重新查询数据
+        }
+        else {
+          //点击了 取消按钮
+          // console.log( '点击取消' )
         }
 
-        this.$store.dispatch( 'UpdateUserSelectQueryType' , _data );
+        this.showdialog = false;
+      } ,
+      onselecttype () {
+        this.showdialog = true;
+
+        // this.$store.dispatch( 'UpdateUserSelectQueryType' , _data );
 
       } ,
       onSearch ( event ) {
@@ -66,6 +126,9 @@
     } ,
     //计算属性
     computed : {
+      radioval1 () {
+        return this.userselectquery.querytype;
+      } ,
       userselectquery () {
         return this.$store.state.userselectquery;
 
