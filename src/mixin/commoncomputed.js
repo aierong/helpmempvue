f6b3cd5b1b55295fb30c9bde6c1da94165eee88c
aierong <aierong@126.com>
2019-06-16 22:30:39 +0800

    home

--- src/mixin/commoncomputed.js
+++ src/mixin/commoncomputed.js
@@ -4,14 +4,18 @@
  Time: 10:27
  功能: js脚本
  */
+import { mapGetters } from 'vuex'
 
 export const commoncomputed = {
   //
   computed : {
-    //是保存成功状态
-    getissaveok () {
-      return this.$store.getters.getissaveok;
-    } ,
+
+    ...mapGetters( {
+      //是保存成功状态
+      // 如果你想将一个 getter 属性另取一个名字，使用对象形式
+      getissaveok : 'getissaveok'
+
+    } ) ,
   } ,
 
 }
